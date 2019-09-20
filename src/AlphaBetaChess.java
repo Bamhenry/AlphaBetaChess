2313127a6df0013b16cca063da358d4652fa6f1b
Bamhenry <32941634+Bamhenry@users.noreply.github.com>
2019-09-20 15:01:43 -0500

    added possible moves for rook and bishop, assuming king is safe

--- src/AlphaBetaChess.java
+++ src/AlphaBetaChess.java
@@ -8,7 +8,7 @@ public class AlphaBetaChess {
         {" "," "," "," "," "," "," "," "},
         {" "," "," "," "," "," "," "," "},
         {" "," "," "," "," "," "," "," "},
-        {" "," "," "," "," "," "," "," "},
+        {"R"," "," "," "," "," "," "," "},
         {"P","P","P","P","P","P","P","P"},
         {"R","K","B","Q","A","B","K","R"}};
     
@@ -61,7 +61,85 @@ public class AlphaBetaChess {
     }
     //possible moves for rook
     public static String possibleR(int i) {
-        String list="";
+        String list="", oldPiece;
+        int r=i/8, c=i%8;
+        int temp=1;
+        
+        //vertical moves
+        for (int j=-1; j<=1; j+=2) {
+            try {
+                while (" ".equals(chessBoard[r][c+temp*j])) {
+                    //move rook from it's current location
+                    oldPiece=chessBoard[r][c+temp*j];
+                    //set old loaction to nothing
+                    chessBoard[r][c]=" ";
+                    //set new location to rook
+                    chessBoard[r][c+temp*j]="R";
+                    if (kingSafe()){
+                            //assume king is safe for now
+                            list=list+r+c+r+(c+temp*j)+oldPiece;
+                     }                 
+                    //if king is save then record move
+                    chessBoard[r][c]="R";
+                    chessBoard[r][c+temp*j]=oldPiece;
+                    temp++;
+                }
+                //if rook can capture
+                if (Character.isLowerCase(chessBoard[r][c+temp*j].charAt(0))){
+                    //move rook from it's current location
+                    oldPiece=chessBoard[r][c+temp*j];
+                    //set old loaction to nothing
+                    chessBoard[r][c]=" ";
+                    //set new location to rook
+                    chessBoard[r][c+temp*j]="R";
+                    if (kingSafe()){
+                            //assume king is safe for now
+                            list=list+r+c+r+(c+temp*j)+oldPiece;
+                     }                 
+                    //if king is save then record move
+                    chessBoard[r][c]="R";
+                    chessBoard[r][c+temp*j]=oldPiece;
+                }
+            } catch (Exception e) {}
+            temp=1;
+            
+            //horizontal moves
+            try {
+                while (" ".equals(chessBoard[r+temp*j][c])) {
+                    //move rook from it's current location
+                    oldPiece=chessBoard[r+temp*j][c];
+                    //set old loaction to nothing
+                    chessBoard[r][c]=" ";
+                    //set new location to rook
+                    chessBoard[r+temp*j][c]="R";
+                    if (kingSafe()){
+                            //assume king is safe for now
+                            list=list+r+c+(r+temp*j)+c+oldPiece;
+                     }                 
+                    //if king is save then record move
+                    chessBoard[r][c]="R";
+                    chessBoard[r+temp*j][c]=oldPiece;
+                    temp++;
+                }
+                //if rook can capture
+                if (Character.isLowerCase(chessBoard[r+temp*j][c].charAt(0))){
+                    //move rook from it's current location
+                    oldPiece=chessBoard[r+temp*j][c];
+                    //set old loaction to nothing
+                    chessBoard[r][c]=" ";
+                    //set new location to rook
+                    chessBoard[r+temp*j][c]="R";
+                    if (kingSafe()){
+                            //assume king is safe for now
+                            list=list+r+c+(r+temp*j)+c+oldPiece;
+                     }                 
+                    //if king is save then record move
+                    chessBoard[r][c]="R";
+                    chessBoard[r+temp*j][c]=oldPiece;
+                }
+            } catch (Exception e) {}
+        }
+        
         return list;
     }
     //possible moves for knight
@@ -71,7 +149,46 @@ public class AlphaBetaChess {
     }
     //possible moves for bishop
     public static String possibleB(int i) {
-        String list="";
+        String list="", oldPiece;
+        int r=i/8, c=i%8;
+        int temp=1;
+        //enable bishop to move diagonally
+        for (int j=-1; j<=1; j+=2){
+            for (int k=-1; k<=1; k+=2){
+                try {
+                    while(" ".equals(chessBoard[r+temp*j][c+temp*k])) {
+                        //where bishop is going
+                        oldPiece=chessBoard[r+temp*j][c+temp*k];
+                        //make current location blank
+                        chessBoard[r][c]=" ";
+                        //update new spot
+                        chessBoard[r+temp*j][c+temp*k]="B";
+                        if (kingSafe()){
+                            //assume king is safe for now
+                            list=list+r+c+(r+temp*j)+(c+temp*k)+oldPiece;
+                        }
+                        chessBoard[r][c]="B";
+                        chessBoard[r+temp*j][c+temp*k]=oldPiece;
+                        temp++;
+                    }
+                    if (Character.isLowerCase(chessBoard[r+temp*j][c+temp*k].charAt(0))) {
+                        oldPiece=chessBoard[r+temp*j][c+temp*k];
+                        //make current location blank
+                        chessBoard[r][c]=" ";
+                        //update new spot
+                        chessBoard[r+temp*j][c+temp*k]="B";
+                        if (kingSafe()){
+                            //assume king is safe for now
+                            list=list+r+c+(r+temp*j)+(c+temp*k)+oldPiece;
+                        }
+                        chessBoard[r][c]="B";
+                        chessBoard[r+temp*j][c+temp*k]=oldPiece;
+                        
+                    }
+                } catch (Exception e){}
+                temp=1;
+            }
+        }
         return list;
     }
     //possible moves for queen
@@ -82,49 +199,50 @@ public class AlphaBetaChess {
         //enable queen to move in all eight directions
         for (int j=-1; j<=1; j++){
             for (int k=-1; k<=1; k++){
-                try {
-                    while(" ".equals(chessBoard[r+temp*j][c+temp*k])) {
-                        //where queen is going
-                        oldPiece=chessBoard[r+temp*j][c+temp*k];
-                        //make current location blank
-                        chessBoard[r][c]=" ";
-                        //update new spot
-                        chessBoard[r+temp*j][c+temp*k]="Q";
-                        if (kingSafe()){
-                            //assume king is safe for now
-                            list=list+r+c+(r+temp*j)+(c+temp*k)+oldPiece;
+                if (j!=0 || k!=0) {
+                    try {
+                        while(" ".equals(chessBoard[r+temp*j][c+temp*k])) {
+                            //where queen is going
+                            oldPiece=chessBoard[r+temp*j][c+temp*k];
+                            //make current location blank
+                            chessBoard[r][c]=" ";
+                            //update new spot
+                            chessBoard[r+temp*j][c+temp*k]="Q";
+                            if (kingSafe()){
+                                //assume king is safe for now
+                                list=list+r+c+(r+temp*j)+(c+temp*k)+oldPiece;
+                            }
+                            chessBoard[r][c]="Q";
+                            chessBoard[r+temp*j][c+temp*k]=oldPiece;
+                            temp++;
                         }
-                        chessBoard[r][c]="Q";
-                        chessBoard[r+temp*j][c+temp*k]=oldPiece;
-                        temp++;
-                    }
-                    if (Character.isLowerCase(chessBoard[r+temp*j][c+temp*k].charAt(0))) {
-                        oldPiece=chessBoard[r+temp*j][c+temp*k];
-                        //make current location blank
-                        chessBoard[r][c]=" ";
-                        //update new spot
-                        chessBoard[r+temp*j][c+temp*k]="Q";
-                        if (kingSafe()){
-                            //assume king is safe for now
-                            list=list+r+c+(r+temp*j)+(c+temp*k)+oldPiece;
+                        if (Character.isLowerCase(chessBoard[r+temp*j][c+temp*k].charAt(0))) {
+                            oldPiece=chessBoard[r+temp*j][c+temp*k];
+                            //make current location blank
+                            chessBoard[r][c]=" ";
+                            //update new spot
+                            chessBoard[r+temp*j][c+temp*k]="Q";
+                            if (kingSafe()){
+                                //assume king is safe for now
+                                list=list+r+c+(r+temp*j)+(c+temp*k)+oldPiece;
+                            }
+                            chessBoard[r][c]="Q";
+                            chessBoard[r+temp*j][c+temp*k]=oldPiece;
+
                         }
-                        chessBoard[r][c]="Q";
-                        chessBoard[r+temp*j][c+temp*k]=oldPiece;
-                        
-                    }
-                } catch (Exception e){}
-                temp=1;
+                    } catch (Exception e){}
+                    temp=1;
+                }
             }
         }
-        
         return list;
     }
     //possible moves for king
     public static String possibleA(int i) {
         //add castling
         String list="", oldPiece;
-       
         int r=i/8, c=i%8;
+        
         for (int j=0; j<9; j++){
             if (j!=4) {
                 try{
